6dfdf1081db0847f6a8e04e16a4a1776b0e95c91
doniyorpdf <doniyorabduhakimov90@gmail.com>
2022-04-04 21:16:35 +0500

    master

--- base/views.py
+++ base/views.py
@@ -72,7 +72,7 @@ def home(request):
     'room_count':room_count, 'room_messages':room_messages}
     return render(request ,'base/home.html', context)
 
-@login_required
+@login_required(login_url='login')
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
