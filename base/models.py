4236a0f7de438caabc7aaa088bdf0bd630f37b8b
doniyorpdf <doniyorabduhakimov90@gmail.com>
2022-04-04 20:59:26 +0500

    master

--- base/models.py
+++ base/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    name = models.CharField(unique=True, max_length=200, null=True)
+    name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True, blank=True)
     
     avatar = models.ImageField(null=True, default="avatar.svg")
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 
 
 class Topic(models.Model):
